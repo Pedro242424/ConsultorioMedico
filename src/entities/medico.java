bdb2dfbf4d17d395a8cc699362536f2b7b0f32d0
Pedro242424 <pedro.blanco225025@potros.itson.edu.mx>
2024-12-02 21:49:58 -0700

    comentario

--- src/entities/medico.java
+++ src/entities/medico.java
@@ -3,6 +3,56 @@ package entities;
 
 
 public class medico {
-     //hola
-    //aqui comente
+     private int id= 0;
+    private String nombreMedico = new String();  
+    private String fechanaciemiento = new String(); 
+    private int edad = 0; 
+    private String telefono = new String();
+
+    public medico(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombrepaciente() {
+        return nombreMedico;
+    }
+
+    public void setNombrepaciente(String nombrepaciente) {
+        this.nombreMedico = nombrepaciente;
+    }
+
+    public String getFechanaciemiento() {
+        return fechanaciemiento;
+    }
+
+    public void setFechanaciemiento(String fechanaciemiento) {
+        this.fechanaciemiento = fechanaciemiento;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
 }
+
