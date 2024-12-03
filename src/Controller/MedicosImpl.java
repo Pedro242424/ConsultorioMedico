1c7ae7e35b873cd495c177a25be899d50889b463
Pedro242424 <pedro.blanco225025@potros.itson.edu.mx>
2024-12-02 23:13:07 -0700

    medicos

--- src/Controller/MedicosImpl.java
+++ src/Controller/MedicosImpl.java
@@ -4,10 +4,55 @@
  */
 package Controller;
 
+import entities.Medicos;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author kiwic
  */
 public class MedicosImpl {
+    private List<Medicos> Medico = new ArrayList();
     
+    public MedicosImpl(){
+        
+        Medicos _med = new Medicos();
+        
+        _med.setId(0);
+        _med.setNombreMedico("Raul Pimentel Rocha");
+        _med.setFechaNaciemiento("19/02/1976");
+        _med.setEdad(48);
+        _med.setTelefono("622-156-8512");
+        Medico.add(_med);
+  
+        
+        this.Medico.add(_med);
+    }
+    
+    public void agregarMedicamento(Medicos med){
+        if(med != null){
+            this.Medico.add(med);
+        }
+    }
+    public void editarMedicamento(Medicos med, int index){
+        if(med != null){
+        this.Medico.set(index, med);
+        }
+    }
+    public void eliminarMedicamento(int index){
+        if(index > -1){
+        this.Medico.remove(index);
+        }
+    }
+    
+    public List<Medicos>consultarMedicamentosTodos(){
+        return this.Medico;
+    }
+    
+    public Medicos consultarMedicamentosId (int index){
+        return this.Medico.get(index);
+    }
 }
+    
+
